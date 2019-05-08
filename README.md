37f6238eedbd2ac7cfaa61049cde6db5d694ad1b
Worachat Arunothaikrit <worachatarunothaikrit@Worachats-MacBook-Pro.local>
2019-05-08 23:19:35 +0900

    :art: :lipstick: Chagne color and refactor

--- README.md
+++ README.md
@@ -3,15 +3,18 @@
 This repository is for Information Visualization class
 
 ## Assets
- https://sites.google.com/view/kobeinfovis2019/
+
+https://sites.google.com/view/kobeinfovis2019/
 
 ## Homepage
- https://worachatsun.github.io/InfoVis2019/
- 
+
+https://worachatsun.github.io/InfoVis2019/
+
 ## Homework
-  [Week 2, Task 1](https://worachatsun.github.io/InfoVis2019/W02/task1.html)  
-  [Week 2, Task 2](https://worachatsun.github.io/InfoVis2019/W02/task2.html)  
-  [Week 4, Task 1](https://worachatsun.github.io/InfoVis2019/W04/task1/w04_ex01.html)  
-  [Week 4, Task 2](https://worachatsun.github.io/InfoVis2019/W04/task2/w04_ex02.html)
-  [Week 5, Task 1](https://worachatsun.github.io/InfoVis2019/W05/task1.html)
-  [Week 5, Task 2](https://worachatsun.github.io/InfoVis2019/W05/task2.html)
+
+[Week 2, Task 1](https://worachatsun.github.io/InfoVis2019/W02/task1.html)  
+ [Week 2, Task 2](https://worachatsun.github.io/InfoVis2019/W02/task2.html)  
+ [Week 4, Task 1](https://worachatsun.github.io/InfoVis2019/W04/task1/w04_ex01.html)  
+ [Week 4, Task 2](https://worachatsun.github.io/InfoVis2019/W04/task2/w04_ex02.html)  
+ [Week 5, Task 1](https://worachatsun.github.io/InfoVis2019/W05/task1.html)  
+ [Week 5, Task 2](https://worachatsun.github.io/InfoVis2019/W05/task2.html)
--- W05/task1.js
+++ W05/task1.js
@@ -1,4 +1,4 @@
-var vertices = [
+const vertices = [
   [-0.6, -0.6, -0.6],
   [0.6, -0.6, -0.6],
   [0.6, -0.6, 0.6],
@@ -10,77 +10,57 @@ var vertices = [
   [-0.6, 0.6, 0.6]
 ];
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(
+const asstes = [
+  { face: [0, 1, 2], color: 0x0040ff }, //blue
+  { face: [0, 3, 2], color: 0x00bfff },
+  { face: [1, 2, 5], color: 0xcc6600 }, //brown
+  { face: [2, 5, 6], color: 0xb35900 },
+  { face: [1, 5, 0], color: 0x1a1a1a }, //black
+  { face: [0, 5, 4], color: 0x333333 },
+  { face: [0, 3, 4], color: 0x00b300 }, //green
+  { face: [3, 4, 7], color: 0x00cc00 },
+  { face: [2, 3, 7], color: 0xff751a }, //orange
+  { face: [2, 6, 7], color: 0xff8533 },
+  { face: [4, 5, 7], color: 0xb300b3 }, //purple
+  { face: [5, 6, 7], color: 0xe600e6 }
+];
+
+let scene = new THREE.Scene();
+let camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
 
-var renderer = new THREE.WebGLRenderer();
+camera.position.z = 3;
+
+let renderer = new THREE.WebGLRenderer();
 renderer.setSize(500, 500);
 document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.Geometry();
+let geometry = new THREE.Geometry();
+let material = new THREE.MeshBasicMaterial();
 
 vertices.map(ver => {
   const vertice = new THREE.Vector3().fromArray(ver);
   geometry.vertices.push(vertice);
 });
 
-const faces = [];
-var f0 = new THREE.Face3(0, 1, 2);
-var f1 = new THREE.Face3(0, 3, 2);
-var f2 = new THREE.Face3(1, 2, 5);
-var f3 = new THREE.Face3(1, 5, 0);
-var f4 = new THREE.Face3(0, 5, 4);
-var f5 = new THREE.Face3(0, 3, 4);
-var f6 = new THREE.Face3(3, 4, 7);
-var f7 = new THREE.Face3(2, 3, 7);
-var f8 = new THREE.Face3(4, 5, 7);
-var f9 = new THREE.Face3(2, 5, 6);
-var f10 = new THREE.Face3(2, 6, 7);
-var f11 = new THREE.Face3(5, 6, 7);
-geometry.faces.push(f0);
-geometry.faces.push(f1);
-geometry.faces.push(f2);
-geometry.faces.push(f3);
-geometry.faces.push(f4);
-geometry.faces.push(f5);
-geometry.faces.push(f6);
-geometry.faces.push(f7);
-geometry.faces.push(f8);
-geometry.faces.push(f9);
-geometry.faces.push(f10);
-geometry.faces.push(f11);
-// geometry.computeFaceNormals();
-
-var material = new THREE.MeshBasicMaterial();
-material.side = THREE.DoubleSide;
-material.vertexColors = THREE.FaceColors;
-geometry.faces[0].color = new THREE.Color(0xdd7778);
-geometry.faces[1].color = new THREE.Color(0xdda197);
-geometry.faces[2].color = new THREE.Color(0xffa147);
-geometry.faces[3].color = new THREE.Color(0xf9ad4f);
-geometry.faces[4].color = new THREE.Color(0x894633);
-geometry.faces[5].color = new THREE.Color(0xa64a31);
-geometry.faces[6].color = new THREE.Color(0x53bab0);
-geometry.faces[7].color = new THREE.Color(0xadbc8a);
-geometry.faces[8].color = new THREE.Color(0x91164d);
-geometry.faces[9].color = new THREE.Color(0xe76c01);
-geometry.faces[10].color = new THREE.Color(0x249acf);
-geometry.faces[11].color = new THREE.Color(0x353037);
+asstes.map((asset, index) => {
+  const tFace = new THREE.Face3(asset.face[0], asset.face[1], asset.face[2]);
+  geometry.faces.push(tFace);
+  geometry.faces[index].color = new THREE.Color(asset.color);
+});
 
 geometry.computeFaceNormals();
 
-var cube = new THREE.Mesh(geometry, material);
-cube.rotation.x = -0.1;
-cube.rotation.y = 0;
-cube.rotation.z = 0;
-scene.add(cube);
+material.side = THREE.DoubleSide;
+material.vertexColors = THREE.FaceColors;
 
-camera.position.z = 3;
+let cube = new THREE.Mesh(geometry, material);
+
+scene.add(cube);
 
 loop();
 
