e8127c17b13b7d3af4a92a603a8ae3e9eb6135c3
Worachat Arunothaikrit <worachatarunothaikrit@Worachats-MacBook-Pro.local>
2019-05-12 17:03:22 +0900

    :art: complete task 1

--- W06/main01.js
+++ W06/main01.js
@@ -1,37 +1,65 @@
 function main() {
-    var width = 500;
-    var height = 500;
+  var width = 1000;
+  var height = 500;
 
-    var scene = new THREE.Scene();
+  var scene = new THREE.Scene();
 
-    var fov = 45;
-    var aspect = width / height;
-    var near = 1;
-    var far = 1000;
-    var camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera.position.set(0, 0, 5);
-    scene.add(camera);
+  var fov = 45;
+  var aspect = width / height;
+  var near = 1;
+  var far = 1000;
+  var camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.set(0, 0, 5);
+  scene.add(camera);
 
-    var light = new THREE.PointLight();
-    light.position.set(5, 5, 5);
-    scene.add(light);
+  var light = new THREE.PointLight();
+  light.position.set(5, 5, 5);
+  scene.add(light);
 
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(width, height);
-    document.body.appendChild(renderer.domElement);
+  var renderer = new THREE.WebGLRenderer();
+  renderer.setSize(width, height);
+  document.body.appendChild(renderer.domElement);
 
-    var geometry = new THREE.TorusKnotGeometry(1, 0.3, 100, 20);
-    var material = new THREE.MeshLambertMaterial();
+  var geometry = new THREE.TorusKnotGeometry(1, 0.3, 100, 20);
 
-    var torus_knot = new THREE.Mesh(geometry, material);
-    scene.add(torus_knot);
-
-    loop();
-
-    function loop() {
-        requestAnimationFrame(loop);
-        torus_knot.rotation.x += 0.01;
-        torus_knot.rotation.y += 0.01;
-        renderer.render(scene, camera);
+  var material_lam = new THREE.ShaderMaterial({
+    vertexColors: THREE.VertexColors,
+    vertexShader: document.getElementById("shader.vert.lam").text,
+    fragmentShader: document.getElementById("shader.frag").text,
+    uniforms: {
+      light_position: { type: "v3", value: light.position }
     }
+  });
+
+  var material_phong = new THREE.ShaderMaterial({
+    vertexColors: THREE.VertexColors,
+    vertexShader: document.getElementById("shader.vert.phong").text,
+    fragmentShader: document.getElementById("shader.frag").text,
+    uniforms: {
+      light_position: { type: "v3", value: light.position },
+      camera_position: { type: "v3", value: camera.position }
+    }
+  });
+
+  geometry.faces.map(face => (face.color = new THREE.Color(0xbfebee)));
+
+  var torus_knot_lam = new THREE.Mesh(geometry, material_lam);
+  var torus_knot_phong = new THREE.Mesh(geometry, material_phong);
+
+  torus_knot_lam.position.set(-2, 0, 0);
+  torus_knot_phong.position.set(2, 0, 0);
+
+  scene.add(torus_knot_lam);
+  scene.add(torus_knot_phong);
+
+  loop();
+
+  function loop() {
+    requestAnimationFrame(loop);
+    torus_knot_lam.rotation.x += 0.01;
+    torus_knot_lam.rotation.y += 0.01;
+    torus_knot_phong.rotation.x += 0.01;
+    torus_knot_phong.rotation.y += 0.01;
+    renderer.render(scene, camera);
+  }
 }
